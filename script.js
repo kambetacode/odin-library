95a97beec6c1435dd467ee5870a88a674f1cbee7
Kevin Mora <kambetacode@gmail.com>
2023-10-08 16:41:32 -0500

    Added new styles, added new button
    Added new div and styles to make background opaque
    Added new button to handle read status, still working on that, not completed yet

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const pages = document.getElementById('pages')
 const inputs = document.querySelectorAll('input')
 const submit = document.getElementById('submit')
 const bookContainer = document.getElementById('book-card-container')
+const modal = document.getElementById('modal')
+const overlay = document.querySelector('.overlay')
 const myLibrary = [];
 
 
@@ -31,6 +33,7 @@ function addBookToLibrary(newBook) {
         <p>${myLibrary[i].title}</p>
         <p>${myLibrary[i].author}</p>
         <p>${myLibrary[i].pages}</p>
+        <button class="read-button" id="read"></button>
         <button class="remove-button" id="remove">Remove</button>
         `
 
@@ -53,33 +56,42 @@ function handleRemove(target) {
     })
 }
 
+function handleRead() {
+    const readButton = document.querySelectorAll('#read')
+
+    readButton.forEach(item => {
+        item.addEventListener('click', (e) => {
+            console.log(e.target)
+        })
+    })
+}
+
 submit.addEventListener('click' , (e) => {
     e.preventDefault()
 
     let testBook = new Book(title.value , author.value , pages.value)
-
-    form.style.display = 'none'
+    modal.classList.remove('active')
     bookContainer.style.display = 'grid'
-    body.style.backgroundColor = '#F0EEF1'
+    overlay.style.display = 'none'
     inputs.forEach(item => {
         item.value = ''
     })
 
     addBookToLibrary(testBook)
+    handleRead()
     handleRemove()
 } )
 
 newBookBtn.addEventListener('click', (e) => {
-    form.style.display = 'flex'
-    body.style.backgroundColor = '#787778'
-    body.style.height = "100vh"
+    modal.classList.add('active')
+    overlay.style.display = 'block'
     form.style.backgroundColor = '#F0EEF1'
     
 })
 
 body.addEventListener('click', (e) => {
     if(e.target.className != "form") {
-        form.style.display = 'none'
-        body.style.backgroundColor = '#F0EEF1'
+        modal.classList.remove('active')
+        overlay.style.display = 'none'
     }
 })
