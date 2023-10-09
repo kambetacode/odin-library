548b6cf26b0ab6807292c899f569b19f5a200a01
Kevin Mora <kambetacode@gmail.com>
2023-10-09 09:20:35 -0500

    Changes on the entire javascript code and function
    Changes on the javascript code to make it simpler and make it easier to read using classes to initialize objects
    Added some comments to each section on the code to make code cleaner
    Added new functionality to use a for of loop instead of a simple for loop to iterate through the books
    Added new code to add functionality to the read and remove buttons
    Changed html file and css file a little bit to match with the javascript code

--- script.js
+++ script.js
@@ -1,108 +1,160 @@
-const body = document.querySelector('body')
-const newBookBtn = document.getElementById('new-book-btn')
-const newBtnDiv = document.getElementById('button-div')
-const form = document.getElementById('form')
-const title = document.getElementById('title')
-const author = document.getElementById('author')
-const pages = document.getElementById('pages')
-const inputs = document.querySelectorAll('input')
-const submit = document.getElementById('submit')
-const bookContainer = document.getElementById('book-card-container')
-const modal = document.getElementById('modal')
-const overlay = document.querySelector('.overlay')
-const readToggle = document.getElementById('read-toggle')
-const myLibrary = [];
-
-
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function() {
-        return `${title} by ${author}, ${pages}, ${read}`
+class Book {
+    constructor(
+        title = 'Unknown',
+        author = 'Unknown',
+        pages = 0,
+        isRead = false
+    ) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.isRead = isRead
     }
 }
 
-function addBookToLibrary(newBook) {
-    myLibrary.push(newBook)
-    bookContainer.innerHTML = ''
-
-    for (let i = 0; i < myLibrary.length; i++) {
-        let content = document.createElement('div')
-        content.innerHTML = `
-        <p>${myLibrary[i].title}</p>
-        <p>${myLibrary[i].author}</p>
-        <p>${myLibrary[i].pages}</p>
-        ${readTest(myLibrary[i].read)}
-        <button class="remove-button" id="remove">Remove</button>
-        `
-
-        content.setAttribute('data-index', `${i}`)
-        content.className = 'book-card'
-
-        bookContainer.appendChild(content)
+class Library { 
+    constructor() {
+        this.books = []
     }
-}
 
-function handleRemove() {
-    const remove = document.querySelectorAll('#remove')
-    
-    remove.forEach(item => {
-        item.addEventListener('click', (e) => {
-            let removeParent = item.parentNode
-            removeParent.remove()
-            myLibrary.splice(removeParent.dataset.index, '1')
-        })
-    })
-}
-
-function handleRead() {
-    const readButton = document.querySelectorAll('#read')
-    
-    readButton.forEach(item => {
-
-        item.addEventListener('click', (e) => {
-            let readParent = item.parentNode
-            console.log(e.target)
-        })
-    })
-}
-
-function readTest(readStatus) {
-    if( readStatus === true) {
-        return '<button class="read-button green" id="read">Read</button>'
-    } else {
-        return '<button class="read-button red" id="read">Not read</button>'
+    addBook(newBook) {
+        this.books.push(newBook)
     }
+
+    removeBook(title) {
+        this.books = this.books.filter((book) => book.title !== title)
+    }
+
+    getTitle(title) {
+        return this.books.find(book => book.title === title)
+    }
+
 }
 
-submit.addEventListener('click' , (e) => {
+const library = new Library()
+
+// Interface
+
+const addBookBtn = document.getElementById('add-book-btn')
+const addBookModal = document.getElementById('add-book-modal')
+const overlay = document.getElementById('overlay')
+const addBookForm = document.getElementById('add-book-form')
+const bookCardContainer = document.getElementById('book-card-container')
+
+//
+
+// Handling book input modal
+
+const openAddBookModal = () => {
+    addBookModal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+const closeAddBookModal = () => {
+    addBookForm.reset()
+    addBookModal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+//
+
+// Handling input data 
+
+const getBookFromInput = () => {
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const pages = document.getElementById('pages').value
+    const isRead = document.getElementById('is-read').checked
+
+    return new Book(title, author, pages, isRead)
+}
+
+const addNewBook = (e) => {
     e.preventDefault()
+    const newBook = getBookFromInput()
+    library.addBook(newBook)
+    updateBookContainer()
+    closeAddBookModal()
+}
 
-    let testBook = new Book(title.value , author.value , pages.value, readToggle.checked)
-    modal.classList.remove('active')
-    bookContainer.style.display = 'grid'
-    overlay.style.display = 'none'
-    inputs.forEach(item => {
-        item.value = ''
-    })
+//
 
-    addBookToLibrary(testBook)
-    handleRead()
-    handleRemove()
-} )
+// Handling book cards
 
-newBookBtn.addEventListener('click', (e) => {
-    modal.classList.add('active')
-    overlay.style.display = 'block'
-    form.style.backgroundColor = '#F0EEF1'
-    
-})
+const createBookCard = (book) => {
+    const bookCard = document.createElement('div')
+    const title = document.createElement('p')
+    const author = document.createElement('p')
+    const pages = document.createElement('p')
+    const btnContainer = document.createElement('div')
+    const readBtn = document.createElement('button')
+    const removeBtn = document.createElement('button')
 
-body.addEventListener('click', (e) => {
-    if(e.target.className != "form") {
-        modal.classList.remove('active')
-        overlay.style.display = 'none'
+    bookCard.classList.add('book-card')
+    btnContainer.classList.add('button-group')
+    readBtn.classList.add('btn')
+    removeBtn.classList.add('btn')
+    readBtn.onclick = toggleRead
+    removeBtn.onclick = removeBook
+
+    title.textContent = `${book.title}`
+    author.textContent = `${book.author}`
+    pages.textContent = `${book.pages}`
+    removeBtn.textContent = 'Remove'
+
+    if(book.isRead) {
+        readBtn.textContent = 'Read'
+        readBtn.classList.add('btn-light-green')
+    } else {
+        readBtn.textContent = 'Not read'
+        readBtn.classList.add('btn-light-red')
     }
-})
+
+    bookCard.appendChild(title)
+    bookCard.appendChild(author)
+    bookCard.appendChild(pages)
+    btnContainer.appendChild(readBtn)
+    btnContainer.appendChild(removeBtn)
+    bookCard.appendChild(btnContainer)
+    bookCardContainer.appendChild(bookCard)
+}
+
+const removeBook = (e) => {
+    const title =  e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll(
+        '"',
+        '')
+
+    library.removeBook(title)
+    updateBookContainer()
+}
+
+const toggleRead = (e) => {
+    const title =  e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll(
+        '"',
+        '')
+    const book = library.getTitle(title)
+    book.isRead = !book.isRead
+    updateBookContainer()
+}
+
+const resetBookContainer = () => {
+    bookCardContainer.innerHTML = ''
+}
+
+const updateBookContainer = () => {
+    resetBookContainer()
+    for(let book of library.books) {
+        createBookCard(book)
+    }
+}
+
+//
+
+
+// Event listeners
+
+addBookForm.onsubmit = addNewBook
+addBookBtn.onclick = openAddBookModal
+overlay.onclick = closeAddBookModal
+
+//
